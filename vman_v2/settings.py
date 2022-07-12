1001485a3e7531120137137b3bb4082f12018690
irabi01 <irabikassim01@gmail.com>
2022-07-12 19:08:58 +0300

    comment postgres db settings.py

--- vman_v2/settings.py
+++ vman_v2/settings.py
@@ -106,16 +106,16 @@ DATABASES = {
 }
 
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': config('DB_NAME'),
-       'USER': config('DB_USER'),
-       'PASSWORD': config('DB_PASSWORD'),
-       'HOST':config('DB_PASSWORD') ,
-       # 'HOST': '',
-       'PORT': config('DB_PORT', cast=int),
-   }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': config('DB_NAME'),
+#        'USER': config('DB_USER'),
+#        'PASSWORD': config('DB_PASSWORD'),
+#        'HOST':config('DB_PASSWORD') ,
+#        # 'HOST': '',
+#        'PORT': config('DB_PORT', cast=int),
+#    }
 # }
 
 # DATABASES = {
